76c7d746eaec58b17272b25f671934c5b53cd153
Dmitriy Fetyukhin <dimaparachute11@icloud.com>
2021-11-22 17:27:47 +0300

    Initial Commit

---
+++ collections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collections
+//
+//  Created by Дмитрий Фетюхин on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
